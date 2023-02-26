15161cadcd4347669c12aa93f98fb589f653d6f1
Pavlo Nechai <nechaipavel7@gmail.com>
2023-02-26 19:25:13 +0200

    second day commit

--- src/scripts/script.js
+++ src/scripts/script.js
@@ -8,3 +8,28 @@ window.addEventListener("scroll", function () {
     navbar.classList.remove("navbar-dark");
   }
 });
+
+// =============================================
+
+// Отримайте всі посилання навігації
+const navLinks = document.querySelectorAll("nav a");
+
+// Переберіть всі розділи на сторінці
+document.querySelectorAll("section").forEach((section) => {
+  // Отримайте верхню границю розділу
+  const sectionTop = section.offsetTop - 50;
+
+  // При скролі до верхньої границі розділу
+  window.addEventListener("scroll", () => {
+    if (window.pageYOffset >= sectionTop) {
+      // Отримайте ідентифікатор розділу
+      const id = section.getAttribute("id");
+
+      // Видаліть клас active з усіх посилань навігації
+      navLinks.forEach((link) => link.classList.remove("active"));
+
+      // Додайте клас active до посилання з відповідним ідентифікатором
+      document.querySelector(`nav a[href="#${id}"]`).classList.add("active");
+    }
+  });
+});
